fd434f057f85f2ec6614df36b5fef0b554cbf9f2
EyronAraza <84764511+EyronAraza@users.noreply.github.com>
2024-06-03 18:04:21 +0400

    changed cors origin link

--- client/src/components/GlobalContext.jsx
+++ client/src/components/GlobalContext.jsx
@@ -3,7 +3,8 @@ import React, { createContext } from 'react'
 const GlobalContext = createContext()
 
 export const GlobalProvider = ({ children }) => {
-    const originUrl = "http://localhost:5000"
+    // const originUrl = "http://localhost:5000"
+    const originUrl = "https://mern-basic-shop-test-api.vercel.app"
 
     return (
         <GlobalContext.Provider value={{ originUrl }}>
